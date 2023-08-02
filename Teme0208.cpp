7b2504f7f1c202e0104931657bc46683873a8b0a
AlexOno7 <onofreialex01@yahoo.com>
2023-08-02 10:36:19 +0300

    Last commit 02.08

--- Teme0208.cpp
+++ Teme0208.cpp
@@ -1,8 +1,9 @@
-//De facut : 10X , 3077X , 65X , 3078X , 4373? , 4383 , 376 , 3663 , 4329 , 1453 , 159
+//De facut : 10X , 3077X , 65X , 3078X , 4373? , 4383? , 376? , 3663X , 4329X , 1453X , 159?
 
 
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int sum() {
@@ -102,12 +103,96 @@ int notaZece() {
 
 
 
+//int Inlocuire() {
+                                            //Problema 4383 - Inlocuire 6 ???
+   
+//}
 
 
 
 
+int sumDiv() {
+                                         //Problema 376 - Suma Divizori? - 90
+    int nr, sum = 0;
+    cin >> nr;
+    for (int i =1;i<=sqrt(nr);i++){
+        if (nr % i == 0) {
+            if (i == nr / i) {
+                sum = sum + i;
+            }
+            else if(i!=nr/i){
+                sum = sum + i + nr / i;
+            }
+        }
+    }
+    return sum;
+}
 
 
+
+int ppdp() {
+                                         //Problema 3663 - ppdp
+    int n, square=0;
+    cin >> n;
+    for (int i = 2; i <= n/2; i++) {
+        if (n % i == 0) {
+            square = i * i;
+            return square;
+            break;
+        }
+    }
+}
+
+
+
+int divizori() {
+                                      //Problema 4329 - Divizori 6
+    int n, sum = 0, minDiv = 0, maxDiv = 0;
+    cin >> n;
+    for (int i = 2; i <= n/2; i++) {
+        if (n % i == 0) {
+            minDiv = i;
+            maxDiv = n / i;
+            sum = minDiv + maxDiv;
+            return sum;
+            break;
+        }
+    }
+}
+
+
+
+
+int stergere() {
+                                            //Problema 1453 - Stergere 1
+    int n, X[1000];
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> X[i];
+    }
+    for (int i = 0; i < n; i++) {
+        if (X[i] % 2 != 0) {
+            cout << X[i] << " ";
+        }
+    }
+    return 0;
+}
+
+
+
+int inserare() {
+                                            //Problema 159 - Inserare Dupa ??
+    int n, X[25];
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> X[i];
+    }
+    for (int i = 0; i < n; i++) {
+        if (X[i] % 2 == 0) {
+        }
+    }
+    return 0;
+}
 
 
 
@@ -134,7 +219,7 @@ int notaZece() {
 int main(){
     
  
-    cout << notaZece();
+   cout << inserare();
 
 
 
